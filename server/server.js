c105f8c113fa59ee71e7fe6a5014622304503036
Alexander James Turinske <a.turinske@gmail.com>
2016-04-28 15:58:04 -0700

    Creates a server using express

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+var port = process.env.PORT || 1337;
+
+if(!module.parent){ 
+  app.listen(port);
+}
+
+
+console.log(port + ' server started!');
+
+module.exports = app;
