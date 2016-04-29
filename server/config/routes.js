15ea03fe4fef1d75177fef0c2fc7e2e723835aee
Alexander James Turinske <a.turinske@gmail.com>
2016-04-28 20:44:03 -0700

    Modify route names

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,6 +5,6 @@ module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
   // user routing
-  app.post('/api/user', userUtils.newUser);
-  app.get('/api/user', userUtils.allUsers);
+  app.post('/api/user/newUser', userUtils.newUser);
+  app.get('/api/user/retrieveUsers', userUtils.allUsers);
 };
