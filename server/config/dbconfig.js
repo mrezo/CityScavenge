5b247c58863f1e87b49039989f328c5d52f070e9
Alexander James Turinske <a.turinske@gmail.com>
2016-04-28 19:36:58 -0700

    Fix bug where Postgres connection string didn't have a user

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/city_scavange';
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/city_scavange';
 
 module.exports = connectionString;
