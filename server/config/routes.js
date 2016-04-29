44312c28dedc1c8e9d8af523f7e67ec394c15e4e
Alexander James Turinske <a.turinske@gmail.com>
2016-04-28 17:39:12 -0700

    Modify routes to point to new utility file

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,10 @@
 var path = require('path');
-var userUtils = require('../utils/userUtils.js');
+var userUtils = require(path.join(__dirname, '../utils/userUtils'));
 
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
   // user routing
-  app.post('/api/user', function() {console.log('post')});
-  app.get('/api/user', function() {console.log('get')});
+  app.post('/api/user', userUtils.newUser);
+  app.get('/api/user', userUtils.allUsers);
 };
