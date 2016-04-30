468b93ef0372370d8b19feef3d8dfd92f28265ea
Alexander James Turinske <a.turinske@gmail.com>
2016-04-30 13:24:27 -0700

    Add a pin for the user and place it on the map

--- client/components/GameWindow.js
+++ client/components/GameWindow.js
@@ -1,18 +1,32 @@
 class GameWindow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentDidMount() {
     const mapOptions = {
-      center: {lat: 37.7836970, lng: -122.4089660},
+      center: { lat: 37.7836970, lng: -122.4089660 },
       zoom: 15,
     };
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    const finalLocationOptions = {
+    // Adds a marker on the map to represent a user
+    const userOptions = {
+      position: { lat: 37.7848606, lng: -122.4130205 },
+      map,
+      title: 'user',
+      label: 'U',
+    };
+    let userMarker = new google.maps.Marker(userOptions);
+
+    // Adds a marker on the map to represent the finish line location
+    const finishLineOptions = {
       position: { lat: 37.7836970, lng: -122.4089660 },
       map,
       title: 'Finish',
+      label: 'F',
     };
-    let marker = new google.maps.Marker(finalLocationOptions);
+    let finishLinemarker = new google.maps.Marker(finishLineOptions);
     this.setState({ map });
   }
 
