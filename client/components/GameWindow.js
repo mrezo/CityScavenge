b783bc34801bacc2934da42f394752cb2b1e0616
Genevieve1722 <gsublette@gmail.com>
2016-04-29 21:34:28 -0700

    create map rendering component

--- client/components/GameWindow.js
+++ client/components/GameWindow.js
@@ -1,9 +1,35 @@
-var GameWindow = ({map}) =>
-<div>
-    <h3>Current Game: FIND THE CROISSANT</h3>
-    <div className="game-map">
-        <img src={map} />
-    </div>
-</div>
+// var GameWindow = ({map}) =>
+// <div>
+//     <h3>Current Game: FIND THE CROISSANT</h3>
+//     <div className="game-map">
+//         <img src={map} />
+//     </div>
+// </div>
+
+
+
+const GameWindow = React.createClass({
+
+        componentDidMount: function () {
+                var mapOptions = {
+                        center: {lat: 37.7836970, lng: -122.4089660},
+                        zoom: 15,
+                },
+                map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                this.setState({map: map});
+        },
+
+        render: function () {
+                var style = {
+                        width: '500px',
+                        height: '500px',
+                };
+
+                return (
+                        <div id='map' style={style}></div>
+                );
+        }
+});
 
 window.GameWindow = GameWindow;
