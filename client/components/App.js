df25d939b1f2faec246182c0e9d8a51ca45b0e02
Alexander James Turinske <a.turinske@gmail.com>
2016-04-30 12:51:39 -0700

    Fix some styling and pass username to props

--- client/components/App.js
+++ client/components/App.js
@@ -2,37 +2,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    //Current state of the app
-    //===================================
+    // Current state of the app
+    // ===================================
     this.state = {
       currentUser: props.game.username,
       userImage: props.game.image,
     };
   }
 
-  //Initial rendering of the app
-  //===================================
-  render(){
+  // Initial rendering of the app
+  // ===================================
+  render() {
     return (
-      <div> 
+      <div>
         <div>
-          <GameHeader user={this.state.currentUser}/>
+          <GameHeader user={this.state.currentUser} />
         </div>
-        <div> 
-          <GameWindow />
+        <div>
+          <GameWindow user={this.state.currentUser} />
         </div>
-      </div> 
-    )
+      </div>
+    );
   }
 }
 
-//Temporary Dummy Data
-//===================================
-window.dummyGameData = 
-  {
-    username: 'The Gray Animal',
-    image: 'http://www.hdiphonewallpaper.com/uploads/image/Animals/Gray%20animal%20wallpaper.jpg',
-  };
+// Temporary Dummy Data
+// ===================================
+window.dummyGameData = {
+  username: 'The Gray Animal',
+  image: 'http://www.hdiphonewallpaper.com/uploads/image/Animals/Gray%20animal%20wallpaper.jpg',
+};
 
 ReactDOM.render(<App game={dummyGameData} />, document.getElementById('app'));
 
