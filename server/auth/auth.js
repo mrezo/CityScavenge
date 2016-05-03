8f69d6d8171ff255c73a0673c6b21d94afec5bf2
Genevieve1722 <gsublette@gmail.com>
2016-05-02 19:13:11 -0700

    add dependencies in auth file

---
+++ server/auth/auth.js
@@ -0,0 +1,7 @@
+var googleKey = require(__dirname + '/../config/googlemaps.js');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth');
+var User = require(__dirname + '/../utils/userUtils.js');
+
+
+
