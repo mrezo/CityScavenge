0e2f017810cba2c4455b3cf8661bcb27d1395009
Alexander James Turinske <a.turinske@gmail.com>
2016-05-03 16:46:14 -0700

    Build test to connect to database

--- test_server/01_Basic/database.js
+++ test_server/01_Basic/database.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var pg = require('chai');
+var pg = require('pg');
 var path = require('path');
 var connectionString = require(path.join(__dirname, '../../server/config/dbconfig'));
 
@@ -12,13 +12,15 @@ describe('Basic Database Functions', function () {
   describe('Database', function () {
     it('will connect', function (done) {
       pg.connect(connectionString, function(err, client, done) {
-        // SQL Query > Add data
-        var queryAddUser = client.query("INSERT INTO users (username) VALUES ($1)", ['test']);
-        queryAddUser.on('end', function() {
-          expect(result).to.equal('test');
+        if (client === null) {
+          throw new Error('Connection refused, are you sure your postgres is running?');
+        }
+        client.query('SELECT 1+1 AS solution').on('end', function (result) {
           done();
+          expect(typeof result).to.equal('object');
         });
       });
+      done();
     });
     // it('has a user table', function (done) {
     //   db.knex.raw('SHOW TABLES LIKE \'users\'').then(function (response) {
