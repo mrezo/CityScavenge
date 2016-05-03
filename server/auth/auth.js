b0927d0d918e1e2f567ff2164acafa7d328a2a24
Genevieve1722 <gsublette@gmail.com>
2016-05-02 19:24:23 -0700

    add routing for passport and corresponding functions

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -3,5 +3,21 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth');
 var User = require(__dirname + '/../utils/userUtils.js');
 
+// Middleware for checking whether the user is logged in
+module.exports.checkAuth = function (req, res, next) {
+  if (req.session.passport ? req.session.passport.user : false) {
+    next();
+  } else {
+    req.session.error = 'Bad credentials.';
+    res.redirect('/');
+  }
+};
 
+module.exports.handleGoogleLogin = passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/plus.login']
+});
+
+module.exports.authenticateGoogleLogin = passport.authenticate('google', {
+  failureRedirect: '/'
+});
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var userUtils = require(path.join(__dirname, '../utils/userUtils'));
 var finishLineController = require(path.join(__dirname, '../controllers/finishLineController'));
+var auth = require(__dirname + '/../auth/auth.js');
 
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
@@ -12,4 +13,22 @@ module.exports = function (app, express) {
   // below routes are no longer in use
   app.get('/api/geo/gamestart', finishLineController.searchGoogle);
   app.post('/api/geo/distance', finishLineController.getDistance);
+
+  // auth routes
+  app.post('/api/login', auth.checkAuth);
+
+  app.get('/auth/google', auth.handleGoogleLogin);
+
+  app.get('/auth/google/callback', auth.authenticateGoogleLogin,
+    function(req, res) {
+      res.redirect('/home');
+    }
+  );
+
+  app.get('/auth/logout', function(req, res) {
+    req.session.destroy(function() {
+      res.redirect('/');
+    });
+  });
+
 };
