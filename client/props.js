665077fcc5c06ac24c2ccac8e0d69c1f2c5f71f4
Alexander James Turinske <a.turinske@gmail.com>
2016-05-02 19:14:17 -0700

    Export mapStateToProps from props file

--- client/props.js
+++ client/props.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-module.exports = ((state) => {
+export function mapStateToProps(state) {
   return {
     currentMap: state.currentMap,
     username: state.currentUser.username,
     userImage: state.currentUser.image,
   };
-})();
+};
