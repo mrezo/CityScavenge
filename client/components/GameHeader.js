f904520acf327ed5d1ad5a3164d9a62aad2cf8ff
Alexander James Turinske <a.turinske@gmail.com>
2016-05-02 19:17:09 -0700

    Abstract props to other file

--- client/components/GameHeader.js
+++ client/components/GameHeader.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// the first of two things that a React-Redux component exports is
+// You have to write a function that returns an object which
+// tells React-Redux how to map everything in the store to any
+// props refered to in the above component
+// I have abstracted this out into a different file because we
+//   will use the same props for many pages
+import { mapStateToProps } from '../props';
+
+// The first of two things that a React-Redux component exports is
 // a standard React component which uses a bunch of props.
 export class Header extends React.Component {
   render() {
     return (
       <div>
           <button type="button">Start Game</button>
-          <span className="user-name">Current User: { this.props.user } </span>
+          <span className="user-name">Current User: { this.props.username } </span>
       </div>
     );
   }
-}
-
-// Next, you have to write a function that returns an object which
-// tells React-Redux how to map everything in the store to any
-// props refered to in the above component
-function mapStateToProps(state) {
-  return {
-  };
 }
 
 // Lastly, we export an object which tells what function to use to map
