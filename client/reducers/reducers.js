862d5ba5add8d0ceebe74959470630e8dbeb6a33
Alexander James Turinske <a.turinske@gmail.com>
2016-05-03 14:38:57 -0700

    Fully remove the Immutable library syntax

--- client/reducers/reducers.js
+++ client/reducers/reducers.js
@@ -1,23 +1,13 @@
-// Map is almost just like a normal object but it is immutable
-// by nature.  Using these objects prevents us from mutating the
-// state accidentally.
-// import {Map, List} from 'immutable';
 // Helper function for below
-
 function signInAttempt(state, action) {
   // ideally below I would somehow change the path to only show the encounter
   console.log('sign in attempted');
-  return state; // must ALWAYS return state
+  // must ALWAYS return state, or state will become undefined
+  return state;
 }
 
 function signUpAttempt(state, action) {
-  // ideally below I would somehow change the path to only show the encounter
   console.log('sign up attempted');
-  return state; // must ALWAYS return state, or state will become undefined
-}
-
-function createNewEncounter(state, action) {
-  console.log('new encounter created');
   return state;
 }
 
@@ -29,30 +19,39 @@ function createNewEncounter(state, action) {
 //   will be considered the next state
 
 // TODO replace actions below with our requests
+// Below are examples of reducers being used
 export default (state = [], action) => {
   switch (action.type) {
-    case 'SET_STATE':
-      return state.merge(action.state);
     case 'CHANGE_USER_NAME':
-      return state.merge({ 'user': { username: action.username } });
-    case 'GO_TO_ENCOUNTER':
-      return state.set('encounter', action.state.encounter);
+      return [...state, {
+        user: {
+          username: action.username,
+        },
+      }];
     case 'SIGN_IN_ATTEMPT':
       return signInAttempt(state, action);
     case 'SIGN_UP_ATTEMPT':
       return signUpAttempt(state, action);
     case 'SIGNOUT':
-      return state.set('user', {});
+      return [...state, {
+        user: {},
+      }];
     case 'SIGN_IN_FAIL':
-      return state.set('errorMessage', action.message);
+      return [...state, {
+        errorMessage: action.message,
+      }];
     case 'SIGN_UP_FAIL':
-      return state.set('errorMessage', action.message);
+      return [...state, {
+        errorMessage: action.message,
+      }];
     case 'CLEAR_ERRORS':
-      return state.set('errorMessage', '');
-    case 'CREATE_NEW_ENCOUNTER':
-      return createNewEncounter(state, action);
+      return [...state, {
+        errorMessage: '',
+      }];
     case 'GET_ALL_COMMENTS':
-      return state.set('comments', action.comments);
+      return [...state, {
+        comments: action.comments,
+      }];
   }
   window.state = state;
   return state;
