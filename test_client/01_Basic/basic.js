cdd40adb087307c8e940acb893fc0b962da8bb17
Alexander James Turinske <a.turinske@gmail.com>
2016-05-04 09:26:24 -0700

    Get example client tests

--- test_client/01_Basic/basic.js
+++ test_client/01_Basic/basic.js
@@ -1,74 +1,75 @@
+import expect from 'expect';
+import * as actions from '../../client/TodoActions';
+import * as types from '../../constants/ActionTypes';
+import reducer from '../../reducers/todos';
 
-var checkElements = function(selector, elements) {
-  return function() {
-    casper.then(function() {
-      elements.forEach(function(el) {
-        ( selector + ' ' + el ).should.be.inDOM;
-      });
-    });
-  };
-};
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    const text = 'Finish docs';
+    const expectedAction = {
+      type: types.ADD_TODO,
+      text,
+    };
+    expect(actions.addTodo(text)).toEqual(expectedAction);
+  });
+});
 
-var checkElement = function(selector) {
-  return function() {
-    casper.then(function() {
-      selector.should.be.inDOM;
-    });
-  };
-};
-
-casper.start('http://localhost:1337');
-describe('Home page', function() {
-  describe('Basic Navigation', function() {
-    casper.waitForSelector('nav', function() {
-        'nav'.should.be.inDOM;
-    });
-
-    it('should have basic navigation', checkElements('nav', 
-          ['.home-link', 
-          '.signin-link',
-          '.signup-link',
-          '.share-new-encounter-link']));
-
-    it('should have a Faunadex header', function() {
-      casper.then(function() {
-        'header h1'.should.be.inDOM;
-        'header h1'.should.contain.text('Faunadex');
-      });
-    });
+describe('todos reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0,
+      },
+    ]);
   });
 
-  describe('Basic Authentication', function() {
-    it('clicking signin link should move to signin form', function() {
-      casper.thenClick('.signin-link a', function() {
-        casper.waitForSelector('button', function() {
-          'button'.should.contain.text('Sign In');
+  it('should handle ADD_TODO', () => {
+    expect(
+      reducer([], {
+        type: types.ADD_TODO,
+        text: 'Run the tests',
+      })
+    ).toEqual(
+      [
+        {
+          text: 'Run the tests',
+          completed: false,
+          id: 0,
+        },
+      ]
+    );
 
-        });
-      });
-    });
-
-    it('filling in form fields and clicking should log the user in', function() {
-      casper.then(function(){
-        this.fillSelectors('form.sign-in', {
-          'input#username' : 'vanessa',
-          'input#password' : 'vanessa' 
-        });
-        // this.echo('Filling in details');
-      });
-
-      casper.thenClick('button', function() {
-        // this.echo('->Clicked on Login');
-      });   
-
-    });
-
-    it('should have signout-link when signed in', function() {
-      casper.then(function() {
-        this.waitForSelector('.signout-link', function() {
-          '.signout-link'.should.be.inDOM;
-        });
-      });
-    });
+    expect(
+      reducer(
+        [
+          {
+            text: 'Use Redux',
+            completed: false,
+            id: 0,
+          },
+        ],
+        {
+          type: types.ADD_TODO,
+          text: 'Run the tests',
+        }
+      )
+    ).toEqual(
+      [
+        {
+          text: 'Run the tests',
+          completed: false,
+          id: 1,
+        },
+        {
+          text: 'Use Redux',
+          completed: false,
+          id: 0,
+        },
+      ]
+    );
   });
 });
