55e9dfd538b70b9626c7bf782af6dd4cd41f34d8
Alexander James Turinske <a.turinske@gmail.com>
2016-05-03 17:31:02 -0700

    Fix bug where database test didn't actually work

--- test_server/01_Basic/database.js
+++ test_server/01_Basic/database.js
@@ -3,61 +3,33 @@ var pg = require('pg');
 var path = require('path');
 var connectionString = require(path.join(__dirname, '../../server/config/dbconfig'));
 
-// var db = require('../../server/config/db.js');
-// var User = require('../../server/models/user.js');
-
 var serverURL = 'http://127.0.0.1:1337';
+var query = function(text, values, cb) {
+  pg.connect(connectionString, function(err, client, done) {
+    if (client === null) {
+      throw new Error('Connection refused, are you sure your postgres is running?');
+    }
+    client.query(text, values, function (err, result) {
+        done();
+        cb(err, result);
+    });
+  });
+};
 
 describe('Basic Database Functions', function () {
   describe('Database', function () {
     it('will connect', function (done) {
-      pg.connect(connectionString, function(err, client, done) {
-        if (client === null) {
-          throw new Error('Connection refused, are you sure your postgres is running?');
-        }
-        client.query('SELECT 1+1 AS solution').on('end', function (result) {
-          done();
-          expect(typeof result).to.equal('object');
-        });
+      query('SELECT 1+1 AS solution', {}, function (err, result) {
+        expect(typeof result).to.equal('object');
+        done();
       });
-      done();
     });
-    // it('has a user table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'users\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
-    // it('has a encounter table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'encounters\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
-    // it('has a forum table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'forums\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
-    // it('has a picture table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'pictures\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
-    // it('has a post table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'posts\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
-    // it('has a users_friend table', function (done) {
-    //   db.knex.raw('SHOW TABLES LIKE \'users_friends\'').then(function (response) {
-    //     expect(response[0].length).to.equal(1);
-    //     done();
-    //   });
-    // });
+    it('has a user table', function (done) {
+      query('SELECT * FROM users', {}, function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
   });
 // End describe('basic server functions', function () {
 });
