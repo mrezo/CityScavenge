36ebcf4db2c1622695445f89f442374c22e7c8ca
mrezo <balarezo.m@gmail.com>
2016-05-05 20:32:37 -0700

    Adds reducer for auth

---
+++ client/reducers/auth.js
@@ -0,0 +1,49 @@
+// Helper function for below
+function signInAttempt(state, action) {
+  // ideally below I would somehow change the path to only show the encounter
+  console.log('sign in attempted');
+  // must ALWAYS return state, or state will become undefined
+  return state;
+}
+
+function signUpAttempt(state, action) {
+  console.log('sign up attempted');
+  return state;
+}
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'CHANGE_USER_NAME':
+      return [...state, {
+        user: {
+          username: action.username,
+        },
+      }];
+    case 'SIGN_IN_ATTEMPT':
+      return signInAttempt(state, action);
+    case 'SIGN_UP_ATTEMPT':
+      return signUpAttempt(state, action);
+    case 'SIGNOUT':
+      return [...state, {
+        user: {},
+      }];
+    case 'SIGN_IN_FAIL':
+      return [...state, {
+        errorMessage: action.message,
+      }];
+    case 'SIGN_UP_FAIL':
+      return [...state, {
+        errorMessage: action.message,
+      }];
+    case 'CLEAR_ERRORS':
+      return [...state, {
+        errorMessage: '',
+      }];
+    case 'GET_ALL_COMMENTS':
+      return [...state, {
+        comments: action.comments,
+      }];
+    default:
+      return state;
+  }
+};
