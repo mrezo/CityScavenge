e0ae92be3d29b3383ff479f8cf18b4aed7441fa6
Alexander James Turinske <a.turinske@gmail.com>
2016-05-06 17:45:21 -0700

    Create a check point reducer

---
+++ client/reducers/checkPoints.js
@@ -0,0 +1,12 @@
+const checkPoints = (state = {}, action) => {
+  switch (action.type) {
+    case 'PLACE_CHECKPOINT':
+      return {};
+    case 'CHECKPOINT_COLLISION':
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default checkPoints;
