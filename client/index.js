98e8601a767791d44fb7010f62a10fcf35634aff
Alexander James Turinske <a.turinske@gmail.com>
2016-05-06 17:43:41 -0700

    Add to the initial state for more reducers

--- client/index.js
+++ client/index.js
@@ -8,6 +8,49 @@ import cityHunt from './reducers/index';
 const store = createStore(cityHunt, {
   auth: {},
   leftNav: { visible: false },
+  mapReducer: {
+    map: 0,
+    centerLat: 0,
+    centerLng: 0,
+    zoom: 15,
+  },
+  checkpoints: [
+    {
+      id: 0,
+      title: 'Fun',
+      lat: 0,
+      lng: 0,
+      users: [
+        {
+          title: 'Michael',
+          collision: false,
+          timeIn: 0,
+        },
+      ],
+    },
+  ],
+  finishPoint: {
+    lat: 0,
+    lng: 0,
+    users: [
+      {
+        title: 'Michael',
+        collision: false,
+        timeIn: 0,
+      },
+    ],
+  },
+  users: [
+    {
+      title: 'Michael',
+      google_id: '1',
+      lat: 0,
+      lng: 0,
+      map: 0,
+      label: 'U',
+      marker: 0,
+    },
+  ],
 });
 
 ReactDOM.render(
