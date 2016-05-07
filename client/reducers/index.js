5b858ea56352ca9dbb96f16faece4f93ae235761
Alexander James Turinske <a.turinske@gmail.com>
2016-05-06 17:46:21 -0700

    Combine the new reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import mapReducer from './map';
+import users from './users';
+import checkPoints from './checkPoint';
+import finishPoint from './finishPoint';
 import leftNav from './leftNav';
 
 const cityHunt = combineReducers({
   auth,
   leftNav,
+  mapReducer,
+  checkPoints,
+  finishPoint,
+  users,
 });
 
 export default cityHunt;
