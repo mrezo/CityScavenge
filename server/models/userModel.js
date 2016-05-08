2153d5d81ab3690fc78d85da489541be0e26e73c
Alexander James Turinske <a.turinske@gmail.com>
2016-05-08 15:15:41 -0700

    Create a findOrCreate method

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -12,17 +12,17 @@ module.exports = {
       }
       var values = '' + value;
       // SQL Query > Retrieve data
-      var queryAddUser = client.query('SELECT * FROM users '
-        + 'WHERE $1 = $2', [field, values]);
+      var queryFindUser = client.query("SELECT * FROM users WHERE google_id = '114510016389042396254'");
+      // var queryAddUser = client.query('SELECT * FROM users '
+      //   + "WHERE $1 = '" + values "'", [field], function(err, result) {
+      //     console.log('yiss: ', result.row[0]);
+      //   });
       var results = [];
-      console.log('query: ', queryAddUser);
       // Stream results back one row at a time
-      queryAddUser.on('row', function (row) {
-        console.log('row:  ============= ', row);
+      queryFindUser.on('row', function (row) {
         results.push(row);
       });
-      queryAddUser.on('end', function () {
-        console.log('results: ', results);
+      queryFindUser.on('end', function () {
         done();
         return cb(null, results);
       });
@@ -49,6 +49,30 @@ module.exports = {
           done();
           return cb(null, result);
         });
+      });
+    });
+  },
+  findOrCreate: function (displayname, googleid, name, cb) {
+    pg.connect(connectionString, function (err, client, done) {
+      if (err) {
+        done();
+        console.log('createUser error: ', err);
+      }
+      client.query("SELECT * FROM users WHERE "
+      + "google_id = '114510016389042396254'", [], function (err, result) {
+        if (!result.rows[0]) {
+          client.query('INSERT INTO users '
+          + '(displayname, google_id, name) '
+          + 'VALUES ($1, $2, $3)', [displayname, googleid, name], function (err, results) {
+            client.query("SELECT * FROM users WHERE "
+            + "google_id = '114510016389042396254'", [], function (err, user) {
+              done();
+              return cb(null, user.rows[0]);
+            });
+          });
+        }
+        done();
+        return cb(null, result.rows[0]);
       });
     });
   },
