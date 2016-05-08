035c5569301775b5ba04b25accae6cf674e2bbe8
Alexander James Turinske <a.turinske@gmail.com>
2016-05-07 18:31:59 -0700

    Attempt to fix database error

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,52 +3,53 @@ var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../config/dbconfig'));
 
 module.exports = {
-  users: {
-    findUser: function(field, value, cb) {
-      pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if (err) {
-          done();
-          console.log('findUser error: ', err);
-        }
-
-        // SQL Query > Retrieve data
-        var queryAddUser = client.query('SELECT * FROM users '
-          + 'WHERE $1 = $2', [field, value]);
-        var results = [];
-        // Stream results back one row at a time
-        queryAddUser.on('row', function (row) {
-          results.push(row);
+  findUser: function(field, value, cb) {
+    pg.connect(connectionString, function(err, client, done) {
+      // Handle connection errors
+      if (err) {
+        done();
+        console.log('findUser error: ', err);
+      }
+      var values = '' + value;
+      // SQL Query > Retrieve data
+      var queryAddUser = client.query('SELECT * FROM users '
+        + 'WHERE $1 = $2', [field, values]);
+      var results = [];
+      console.log('query: ', queryAddUser);
+      // Stream results back one row at a time
+      queryAddUser.on('row', function (row) {
+        console.log('row:  ============= ', row);
+        results.push(row);
+      });
+      queryAddUser.on('end', function () {
+        console.log('results: ', results);
+        done();
+        return cb(null, results);
+      });
+    });
+  },
+  createUser: function (displayname, googleid, name, cb) {
+    pg.connect(connectionString, function (err, client, done) {
+      if (err) {
+        done();
+        console.log('createUser error: ', err);
+      }
+      var queryAddUser = client.query('INSERT INTO users '
+        + '(displayname, google_id, name) '
+        + 'VALUES ($1, $2, $3)', [displayname, googleid, name]);
+      queryAddUser.on('end', function () {
+        done();
+        var queryRetrieveUser = client.query('SELECT * FROM users '
+          + 'WHERE displayname = $1', [displayname]);
+        var result = [];
+        queryRetrieveUser.on('row', function (row) {
+          result.push(row);
         });
-        queryAddUser.on('end', function () {
+        queryRetrieveUser.on('end', function () {
           done();
-          return cb(null, results);
+          return cb(null, result);
         });
       });
-    },
-    createUser: function (displayname, googleid, name, cb) {
-      pg.connect(connectionString, function (err, client, done) {
-        if (err) {
-          done();
-          console.log('createUser error: ', err);
-        }
-        var queryAddUser = client.query('INSERT INTO users '
-          + '(displayname, google_id, name) '
-          + 'VALUES ($1, $2, $3)', [displayname, googleid, name]);
-        queryAddUser.on('end', function () {
-          done();
-          var queryRetrieveUser = client.query('SELECT * FROM users '
-            + 'WHERE displayname = $1', [displayname]);
-          var result = [];
-          queryRetrieveUser.on('row', function (row) {
-            result.push(row);
-          });
-          queryRetrieveUser.on('end', function () {
-            done();
-            return cb(null, result);
-          });
-        });
-      });
-    },
+    });
   },
 };
