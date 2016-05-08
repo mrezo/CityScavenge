af202881ba07c6cb4b06b78013f83289b50945f1
Alexander James Turinske <a.turinske@gmail.com>
2016-05-07 17:20:33 -0700

    Modify passport routing

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,10 +1,23 @@
 var googleKey = require(__dirname + '/../config/googlemaps.js');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth');
-var User = require(__dirname + '/../utils/userUtils.js');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var User = require('./models/userModel.js');
+var port = process.env.PORT || 1337;
+var callbackServer = '';
 
+if (process.env.SERVER === 'LIVE') {
+  callbackServer = 'http://faunadex.willfulbard.com';
+  if (port !== 80) {
+    callbackServer += ':' + port;
+  }
+} else {
+  callbackServer = 'http://localhost';
+  if (port !== 80) {
+    callbackServer += ':' + port;
+  }
+}
 // Middleware for checking whether the user is logged in
-module.exports.checkAuth = function (req, res, next) {
+exports.checkAuth = function (req, res, next) {
   if (req.session.passport ? req.session.passport.user : false) {
     next();
   } else {
@@ -13,41 +26,42 @@ module.exports.checkAuth = function (req, res, next) {
   }
 };
 
-module.exports.handleGoogleLogin = passport.authenticate('google', {
-  scope: ['https://www.googleapis.com/auth/plus.login']
+exports.handleGoogleLogin = passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/plus.login'],
 });
 
-module.exports.authenticateGoogleLogin = passport.authenticate('google', {
-  failureRedirect: '/'
+exports.authenticateGoogleLogin = passport.authenticate('google', {
+  failureRedirect: '/',
 });
 
 /*
 serializeUser and deserializeUser are two required Passport methods that are
-called when using sessions with Passport. Sessions are saved via cookies 
+called when using sessions with Passport. Sessions are saved via cookies
 rather than via login credentials.
 */
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(function (user, done) {
+  if (user) {
+    User.findUser('google_id', user.id, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      done(err, user);
+    });
+  }
 });
 
-passport.use(new GoogleStrategy.OAuth2Strategy({
+passport.use(new GoogleStrategy({
   clientID: googleKey.CLIENT_ID,
   clientSecret: googleKey.CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-}, function(request, accessToken, refreshToken, profile, done) {
+  callbackURL: callbackServer + '/auth/google/callback',
+}, function (accessToken, refreshToken, profile, done) {
   // Create a user if it is a new user, otherwise just get the user from the DB
-  User
-    .findOrCreate({
-      googleId: profile.id
-    },
-    function(err, user) {
-      return done(err, user);
-    });
-  }));
+  User.findUser('google_id', profile.id, function (err, user) {
+    return done(err, user);
+  });
+}));
