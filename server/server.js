bfb466ae3050af9698c813bc26f4ece13cb55e0f
mrezo <balarezo.m@gmail.com>
2016-05-09 20:30:45 -0700

    Refactors server to use socket.io

--- server/server.js
+++ server/server.js
@@ -4,14 +4,26 @@ var passport = require('passport');
 
 var app = express();
 
+// Required for  Socket.io
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 var port = process.env.PORT || 1337;
 
-if (!module.parent){
-  app.listen(port);
-}
+io.on('connection', function() {
+// this is where the socket listeners go
+
+});
+
+// if (!module.parent) {
+//   app.listen(port);
+// }
+
+//Start the server
+server.listen(port);
 
 console.log(port + ' server started!');
 
