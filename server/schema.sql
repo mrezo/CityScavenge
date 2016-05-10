434c59d86dee1ea7e105d6db59654f13242b991b
Alexander James Turinske <a.turinske@gmail.com>
2016-05-10 12:36:10 -0700

    Update database schema

--- server/schema.sql
+++ server/schema.sql
@@ -5,11 +5,62 @@ CREATE DATABASE city_scavenge;
 \c city_scavenge;
 
 CREATE TABLE users (
-  id SERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
   displayname VARCHAR(63) NOT NULL UNIQUE,
   google_id VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(63) NOT NULL
+  avatar VARCHAR(63) NOT NULL,
+  total_places_visited INTEGER NOT NULL DEFAULT 0,
+  total_distance DOUBLE NOT NULL DEFAULT 0.00,
+  games_played INTEGER NOT NULL DEFAULT 0,
 );
+
+CREATE TABLE game (
+  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+  winning_user INTEGER REFERENCES users,
+  duration TIME NULL DEFAULT NULL,
+  start_time TIME NULL DEFAULT NULL,
+  finish_time TIME NULL DEFAULT NULL,
+  id_task INTEGER NULL DEFAULT NULL,
+);
+   
+CREATE TABLE task (
+  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+  source_user INTEGER REFERENCES users,
+  receiver_user INTEGER REFERENCES users,
+  id_location INTEGER REFERENCES location,
+  action VARCHAR(63) NULL DEFAULT NULL,
+  picture VARCHAR(255) NULL DEFAULT NULL,
+);
+   
+CREATE TABLE location (
+  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+  id_game INTEGER REFERENCES game,
+  latitude DECIMAL NOT NULL DEFAULT 0.0,
+  longitude DECIMAL NOT NULL DEFAULT 0.0,
+  name VARCHAR(63) NULL DEFAULT NULL,
+  picture VARCHAR(63) NULL DEFAULT NULL,
+  collision BOOLEAN NOT NULL DEFAULT false,
+  is_finish_point BOOLEAN NOT NULL DEFAULT false,
+);
+    
+CREATE TABLE game_user (
+  id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+  id_user INTEGER REFERENCES users,
+  id_game INTEGER REFERENCES game,
+);
+
 
 -- Dummy Data
 INSERT INTO users (displayname, google_id, name) VALUES ('Genevieve', '1734', 'Dolphin'), ('Michael', '254', 'Gray Whale'), ('Alexander', '2345', 'Manatee');
+
+-- INSERT INTO `user` (`id`,`google_id`,`name`,`display_name`,`avatar`,`total_places_visited`,`total_distance`,`games_played`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `game` (`id`,`winner`,`duration`,`start_time`,`finish_time`,`id_task`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `task` (`id`,`source_user`,`receiver_user`,`id_location`,`action`,`picture`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `location` (`id`,`id_game`,`id_user`,`latitude`,`longitude`,`name`,`picture`,`collision`,`is_finish_point`) VALUES
+-- ('','','','','','','','','');
+-- INSERT INTO `game_user` (`id`,`id_user`,`id_game`) VALUES
+-- ('','','');
