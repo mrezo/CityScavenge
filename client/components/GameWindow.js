bc449eb3669254ecaa1fc6bb6e353d3136b9e26a
Genevieve1722 <gsublette@gmail.com>
2016-05-10 11:37:01 -0700

    removes unnecessary dependencies

--- client/components/GameWindow.js
+++ client/components/GameWindow.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { mapStateToPropsWindow, mapDispatchToPropsWindow } from '../props';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import IconButton from 'material-ui/lib/icon-button';
-import FontIcon from 'material-ui/lib/font-icon';
-import NavigationExpandMoreIcon from 'material-ui/lib/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/lib/menus/menu-item';
-import DropDownMenu from 'material-ui/lib/DropDownMenu';
 import RaisedButton from 'material-ui/lib/raised-button';
 import Toolbar from 'material-ui/lib/toolbar/toolbar';
 import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
-import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
-import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
 import VisibleDashboardLeftNav from '../containers/VisibleDashboardLeftNav';
 import VisibleTopAppBar from '../containers/VisibleTopAppBar';
 import GoogleMapContainer from '../containers/GoogleMap';
