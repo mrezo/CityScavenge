e4f579f7cbe6c4188959e378b93f16f41744ef93
Genevieve1722 <gsublette@gmail.com>
2016-05-10 11:28:34 -0700

    Adds check in button for checkpoints

--- client/components/GameWindow.js
+++ client/components/GameWindow.js
@@ -24,15 +24,6 @@ const GameWindow = () => (
   <GoogleMapContainer />
     <Toolbar className="bottom-toolbar">
       <ToolbarGroup firstChild={true} float="left">
-        <DropDownMenu value={3}>
-          <MenuItem value={1} primaryText="All Broadcasts" />
-          <MenuItem value={2} primaryText="All Voice" />
-          <MenuItem value={3} primaryText="All Text" />
-          <MenuItem value={4} primaryText="Complete Voice" />
-          <MenuItem value={5} primaryText="Complete Text" />
-          <MenuItem value={6} primaryText="Active Voice" />
-          <MenuItem value={7} primaryText="Active Text" />
-        </DropDownMenu>
       </ToolbarGroup>
       <ToolbarGroup float="right">
         <ToolbarTitle text="Options" />
@@ -48,7 +39,7 @@ const GameWindow = () => (
           <MenuItem primaryText="More Info" />
         </IconMenu>
         <ToolbarSeparator />
-        <RaisedButton label="Create Broadcast" primary={true} />
+        <RaisedButton label="Check In" primary={true} />
       </ToolbarGroup>
     </Toolbar>
   </div>
