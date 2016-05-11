eac12910571d55d3e56d3867f99a1a9327991e5d
mrezo <balarezo.m@gmail.com>
2016-05-11 15:24:17 -0700

    Modifies socket.on createUser to get newUserPosition and dispatches a create user action

--- client/socketIO.js
+++ client/socketIO.js
@@ -1,46 +1,22 @@
 import io from 'socket.io-client';
-import { createUser } from './actions/map.js';
 import { placeUserMarker, deleteUserMarker } from './actions/map';
+import { newUserPosition } from './lib/locationController';
+import { createUser } from './actions/user';
 
 export const socket = io();
 
 export default function (store) {
+  // On user connection create with lat and long
   socket.on('createUser', (data) => {
+    // get initial coords of user
+    newUserPosition((currentLocation) => {
+      console.log('I HAS ACCESS TO INITCOORDS', currentLocation);
+      store.dispatch(createUser(data.title, currentLocation));
+    });
     // =================================================
     // Gets the user coordinates
     // =================================================
 
-    store.dispatch(createUser(data.title));
-
-    const initialPosition = () => {
-      let currentLocation;
-
-      let showLocation = (position) => {
-        currentLocation =
-        {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
-      };
-
-      const geoError = () => {
-        console.log('Finding geolocation failed.');
-      };
-
-      const geoOptions = {
-        enableHighAccuracy: true,
-        maximumAge: 30000,
-        timeout: 27000,
-      };
-
-      const getWatchID = () => navigator.geolocation.getCurrentPosition(showLocation, geoError, geoOptions);
-
-      const watchID = getWatchID();
-      return { currentLocation, watchID };
-    };
-
-    const initCoords = initialPosition();
-    console.log(initCoords);
 
     // update user with coords
     // socket.emit()
@@ -49,40 +25,40 @@ export default function (store) {
     // Updates the user coordinates
     // =================================================
 
-    const getUserLocationAndWatchID = (googleMap, title) => {
-      let currentLocation = {};
+    // const getUserLocationAndWatchID = (googleMap, title) => {
+    //   let currentLocation = {};
 
-      let showLocation = (position) => {
-        console.log('Position', position);
-        currentLocation =
-        {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
-        // TODO: Pass Map as argument
-        store.dispatch(deleteUserMarker(title));
-        store.dispatch(placeUserMarker(googleMap, title, currentLocation));
-        return currentLocation;
-      };
+    //   let showLocation = (position) => {
+    //     console.log('Position', position);
+    //     currentLocation =
+    //     {
+    //       latitude: position.coords.latitude,
+    //       longitude: position.coords.longitude,
+    //     };
+    //     // TODO: Pass Map as argument
+    //     store.dispatch(deleteUserMarker(title));
+    //     store.dispatch(placeUserMarker(googleMap, title, currentLocation));
+    //     return currentLocation;
+    //   };
 
-      const geoError = () => {
-        console.log('Finding geolocation failed.');
-      };
+    //   const geoError = () => {
+    //     console.log('Finding geolocation failed.');
+    //   };
 
-      const geoOptions = {
-        enableHighAccuracy: true,
-        maximumAge: 30000,
-        timeout: 27000,
-      };
+    //   const geoOptions = {
+    //     enableHighAccuracy: true,
+    //     maximumAge: 30000,
+    //     timeout: 27000,
+    //   };
 
-      const getWatchID = () => navigator.geolocation.watchPosition(showLocation, geoError, geoOptions);
+    //   const getWatchID = () => navigator.geolocation.watchPosition(showLocation, geoError, geoOptions);
 
-      const watchID = getWatchID();
-      return { currentLocation, watchID };
-    };
+    //   const watchID = getWatchID();
+    //   return { currentLocation, watchID };
+    // };
 
-    store.dispatch(placeUserMarker(store.getState().mapReducer.map, data.title, coords.currentLocation));
-    getUserLocationAndWatchID(store.getState().mapReducer.map, data.title);
+    // store.dispatch(placeUserMarker(store.getState().mapReducer.map, data.title, coords.currentLocation));
+    // getUserLocationAndWatchID(store.getState().mapReducer.map, data.title);
   });
 }
 
