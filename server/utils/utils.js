d2708583017aa89e875b1496fa60f3428df30bf9
Alexander James Turinske <a.turinske@gmail.com>
2016-05-10 21:17:02 -0700

    Abstract out pg query

---
+++ server/utils/utils.js
@@ -0,0 +1,18 @@
+var pg = require('pg');
+var path = require('path');
+var connectionString = require(path.join(__dirname, '../config/dbconfig'));
+
+module.exports = {
+  query: function(obj, cb) {
+    pg.connect(connectionString, function(err, client, done) {
+      if (err) {
+        done();
+        console.log(err);
+      }
+      client.query(obj.text, obj.values, function (err, result) {
+        done();
+        cb(err, result.rows);
+      });
+    });
+  },
+};
