b04b473dbf82e3f57c6b09c6e3658951df467631
Genevieve1722 <gsublette@gmail.com>
2016-05-10 21:41:38 -0700

    includes dependencies on photo submit container

--- client/containers/PhotoSubmit.js
+++ client/containers/PhotoSubmit.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import PhotoUpload from '../components/PhotoUpload';
+import { photoUploadStart, photoSubmit } from '../actions/map';
 
 PhotoUpload.propTypes = {
   lat: PropTypes.number.isRequired,
