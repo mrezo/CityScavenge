ea49b80f09eb9994d80267f53ac3cd17c427cbb8
Alexander James Turinske <a.turinske@gmail.com>
2016-05-10 21:19:15 -0700

    Update database tests to match new schema

--- test_server/01_Basic/database.js
+++ test_server/01_Basic/database.js
@@ -19,17 +19,46 @@ var query = function(text, values, cb) {
 describe('Basic Database Functions', function () {
   describe('Database', function () {
     it('will connect', function (done) {
-      query('SELECT 1+1 AS solution', {}, function (err, result) {
+      query('SELECT 1+1 AS solution', [], function (err, result) {
         expect(typeof result).to.equal('object');
         done();
       });
     });
-    it('has a user table', function (done) {
-      query('SELECT * FROM users', {}, function(err, result) {
+    it('has a users table', function (done) {
+      query('SELECT * FROM users', [], function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it('has a game table', function (done) {
+      query('SELECT * FROM game', [], function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it('has a game_user table', function (done) {
+      query('SELECT * FROM game_user', [], function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it('has a checkpoint table', function (done) {
+      query('SELECT * FROM checkpoint', [], function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it('has a task table', function (done) {
+      query('SELECT * FROM task', [], function(err, result) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it('has a checkpoint_users table', function (done) {
+      query('SELECT * FROM checkpoint_users', [], function(err, result) {
         expect(err).to.equal(null);
         done();
       });
     });
   });
-// End describe('basic server functions', function () {
 });
