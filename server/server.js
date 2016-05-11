f3c33264c68f0ea569e799f5bc6f723ffd3d9eb4
mrezo <balarezo.m@gmail.com>
2016-05-11 11:19:57 -0700

    Modifies server to make socket connection

--- server/server.js
+++ server/server.js
@@ -1,30 +1,23 @@
 var express = require('express');
 var path = require('path');
 var passport = require('passport');
+var socket = require('./config/socket.js');
 
 var app = express();
 
-// Required for  Socket.io
+// Required for Socket.io
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+socket(server);
+// var io = require('socket.io')(server);
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 var port = process.env.PORT || 1337;
 
-io.on('connection', function() {
-// this is where the socket listeners go
-
-});
-
-// if (!module.parent) {
-//   app.listen(port);
-// }
-
-//Start the server
+// Start the server
 server.listen(port);
 
-console.log(port + ' server started!');
+console.log(port + 'server started!');
 
 module.exports = app;
