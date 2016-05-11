2c2003dd305e6fd6daca6c114efd8d428d715afb
Genevieve1722 <gsublette@gmail.com>
2016-05-10 19:21:47 -0700

    include checkpoints reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -5,13 +5,11 @@ import users from './users';
 import checkpoints from './checkpoints';
 import finishPoint from './finishPoint';
 import leftNav from './leftNav';
-import { reducer as modalReducer } from 'react-redux-modal';
 
 const cityHunt = combineReducers({
   auth,
   leftNav,
   mapReducer,
-  modalReducer,
   checkpoints,
   finishPoint,
   users,
