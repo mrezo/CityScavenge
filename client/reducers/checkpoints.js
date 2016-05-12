613ea825d3874beb5f546f31e3f05622b320dcc6
Genevieve1722 <gsublette@gmail.com>
2016-05-11 19:15:04 -0700

    remove photo upload actions from checkpoints reducer

--- client/reducers/checkpoints.js
+++ client/reducers/checkpoints.js
@@ -4,18 +4,6 @@ const checkpoints = (state = [], action) => {
       return {};
     case 'CHECKPOINT_COLLISION':
       return {};
-    case 'PHOTO_UPLOAD_START':
-      return Object.assign({}, state, {
-        lat: action.lat,
-        lng: action.lng,
-        open: true,
-      });
-    case 'PHOTO_SUBMIT':
-      return Object.assign({}, state, {
-        lat: action.lat,
-        lng: action.lng,
-        open: false,
-      });
     default:
       return state;
   }
