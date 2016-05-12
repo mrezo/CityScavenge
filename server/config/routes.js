4e7bc3a0ba5fd043abeb3c9cbaf91f90a5976098
Alexander James Turinske <a.turinske@gmail.com>
2016-05-11 19:39:27 -0700

    Fix missing forward slash

--- server/config/routes.js
+++ server/config/routes.js
@@ -18,9 +18,9 @@ module.exports = function (app, express) {
 
   // google auth routes
   app.post('/api/login', auth.checkAuth);
-  app.get('api/v1/auth/google', auth.handleGoogleLogin);
+  app.get('/api/v1/auth/google', auth.handleGoogleLogin);
   app.get('/api/v1/logout', auth.logout);
-  app.get('api/v1/auth/google/callback', auth.authenticateGoogleLogin,
+  app.get('/api/v1/auth/google/callback', auth.authenticateGoogleLogin,
     function (req, res) {
       res.redirect('http://localhost:1337/#/dashboard');
     }
