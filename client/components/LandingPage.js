adb39ffec06d790b51a826f25e56e950c5f6ebd5
Alexander James Turinske <a.turinske@gmail.com>
2016-05-11 20:04:06 -0700

    Fix merge issues

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -10,14 +10,9 @@ const LandingPage = () => (
     <card backgroundColor={styles.Colors.cyan300}>
       <CardMedia overlay={<CardTitle title="City Scavenge" subtitle="Explore your city" />}><img src={'https://cdn.getyourguide.com/niwziy2l9cvz/1XBkFZIKqYw0248uGCuaWG/cc3d1c8fec71b6706ac961b449d2d823/san-francisco-san-francisco-bay-1112x630.jpg'} /></CardMedia>
       <div className="button-group">
-        <RaisedButton label="Login with Google" backgroundColor={styles.Colors.red500} primary={true} />
+        <RaisedButton label="Login with Google" backgroundColor={styles.Colors.red500} primary={true} linkButton={true} href="api/v1/auth/google" />
       </div>
     </card>
-    <h3 className="app-title">City Scavenge</h3>
-    <span>Explore your city</span>
-    <div className="button-group">
-      <RaisedButton label="Login with Google" primary={true} linkButton={true} href="api/v1/auth/google" />
-    </div>
   </div>
 );
 
