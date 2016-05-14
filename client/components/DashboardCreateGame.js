80cfb63c4b7d6e5799ae06294480fed8cc6f0223
Alexander James Turinske <a.turinske@gmail.com>
2016-05-13 18:47:02 -0700

    Fix text fields

--- client/components/DashboardCreateGame.js
+++ client/components/DashboardCreateGame.js
@@ -6,17 +6,17 @@ import styles from 'material-ui/lib/styles';
 const DashboardCreateGame = () => (
   <div>
     <TextField
-      hintText="time to type!"
+      hintText="Time to Type!"
       floatingLabelText="Game Name"
       multiLine={true}
       rows={2}
     /><br />
     <TextField
-      hintText="time to type!"
+      hintText="Time to Type!"
       floatingLabelText="Game ID"
     /><br />
     <TextField
-      hintText="Message Field"
+      hintText="Time to Type!"
       floatingLabelText="Game Radius"
     /><br />
     <RaisedButton label="Create Game" backgroundColor={styles.Colors.pink400} primary={true} linkButton={true} href="/#/game" />
