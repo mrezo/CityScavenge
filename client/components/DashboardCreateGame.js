8d3e8db4b4c12e27007b5f8447d8aa9428c6f54d
Alexander James Turinske <a.turinske@gmail.com>
2016-05-13 21:06:12 -0700

    Start to implement text fields in DashboardCreateGames

--- client/components/DashboardCreateGame.js
+++ client/components/DashboardCreateGame.js
@@ -3,19 +3,26 @@ import TextField from 'material-ui/lib/TextField';
 import RaisedButton from 'material-ui/lib/raised-button';
 import styles from 'material-ui/lib/styles';
 
-const DashboardCreateGame = ({ gameName, submitGame }) => (
+var gameRadius = 0;
+
+const DashboardCreateGame = ({ gameName, submitGame, handleChange }) => (
   <div>
     <TextField
       hintText="Time to Type!"
       floatingLabelText="Game Name"
       value={ gameName }
+      onChange={ handleChange }
+      id="text-field-controlled"
+      onChange={ handleChange }
       multiLine={true}
       rows={2}
     /><br />
     <TextField
       hintText="Time to Type!"
+      // id="text-field-controlled"
       floatingLabelText="Game Radius"
       value={ gameRadius }
+      onChange={ handleChange }
     /><br />
     <RaisedButton
       label="Create Game"
@@ -31,6 +38,7 @@ const DashboardCreateGame = ({ gameName, submitGame }) => (
 DashboardCreateGame.propTypes = {
   gameName: PropTypes.string.isRequired,
   submitGame: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default DashboardCreateGame;
