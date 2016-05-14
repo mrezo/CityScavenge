5705d4b57ffc204ec475aad67664e8594d646550
Alexander James Turinske <a.turinske@gmail.com>
2016-05-13 20:42:27 -0700

    Add props and props validation to component

--- client/components/DashboardCreateGame.js
+++ client/components/DashboardCreateGame.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import TextField from 'material-ui/lib/TextField';
 import RaisedButton from 'material-ui/lib/raised-button';
 import styles from 'material-ui/lib/styles';
 
-const DashboardCreateGame = () => (
+const DashboardCreateGame = ({ gameName, submitGame }) => (
   <div>
     <TextField
       hintText="Time to Type!"
       floatingLabelText="Game Name"
-      value={}
+      value={ gameName }
       multiLine={true}
       rows={2}
     /><br />
     <TextField
       hintText="Time to Type!"
       floatingLabelText="Game Radius"
-      value={}
+      value={ gameRadius }
     /><br />
     <RaisedButton
       label="Create Game"
       backgroundColor={styles.Colors.pink400}
       primary={true}
       linkButton={true}
-      onMouseDown{() => {
-
-      }}
+      onMouseDown={ submitGame }
       href="/#/game"
     />
   </div>
 );
+
+DashboardCreateGame.propTypes = {
+  gameName: PropTypes.string.isRequired,
+  submitGame: PropTypes.func.isRequired,
+};
 
 export default DashboardCreateGame;
