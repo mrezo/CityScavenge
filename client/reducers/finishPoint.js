bddadd463ca469680957d5fd36d4ab6bd0aba673
mrezo <balarezo.m@gmail.com>
2016-05-14 12:03:46 -0700

    Creates setFinishPoint action type for reduce

--- client/reducers/finishPoint.js
+++ client/reducers/finishPoint.js
@@ -15,6 +15,11 @@ const user = (state = {}, action) => {
 
 const finishPoint = (state = {}, action) => {
   switch (action.type) {
+    case 'SET_FINISHPOINT':
+      return Object.assign({}, state, {
+        lat: action.lat,
+        lng: action.lng,
+      });
     case 'PLACE_FINISHPOINT':
       const finishPointOptions = {
         position: { lat: action.lat, lng: action.lng },
