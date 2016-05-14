878f0f8d15c63a1eea79ce01d9bfe4c23c995675
Genevieve1722 <gsublette@gmail.com>
2016-05-14 16:07:00 -0700

    updates checkpoints action to accept an array as state

--- client/reducers/checkpoints.js
+++ client/reducers/checkpoints.js
@@ -1,9 +1,21 @@
 const checkpoints = (state = [], action) => {
   switch (action.type) {
     case 'PLACE_CHECKPOINT':
-      return {};
+      const checkpointOptions = {
+        position: { lat: action.lat, lng: action.lng },
+        map: action.map,
+        title: state.title,
+        label: state.label,
+      };
+      return [...state, {
+        checkpoint: {
+          lat: action.lat,
+          lng: action.lng,
+          marker: new google.maps.Marker(checkpointOptions),
+        },
+      }];
     case 'CHECKPOINT_COLLISION':
-      return {};
+      return [];
     default:
       return state;
   }
