981b7bfbca5940b6c1b612bd7a730a0d314d2692
mrezo <balarezo.m@gmail.com>
2016-05-14 12:04:36 -0700

    Corrects spacing on Create Map function

--- client/reducers/mapReducer.js
+++ client/reducers/mapReducer.js
@@ -1,7 +1,6 @@
 const mapReducer = (state = {}, action) => {
   switch (action.type) {
     case 'CREATE_MAP':
-      console.log(googleMap);
       return Object.assign({}, state, {
         map: action.googleMap,
         centerLat: action.lat,
