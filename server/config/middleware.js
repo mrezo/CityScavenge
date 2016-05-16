03f3e51cd02e877e3a4015a7d23ea944a2ce42fb
Alexander James Turinske <a.turinske@gmail.com>
2016-05-16 11:50:10 -0700

    Modify routing to change order and include morgan

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,18 +1,22 @@
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var session = require('express-session')
-var cookieParser = require('cookie-parser')
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
 
 module.exports = function (app, express) {
+  app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(cookieParser());
-  app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  }));
   app.use(passport.initialize());
+  app.use(session(
+    {
+      secret: 'keyboard cat',
+      resave: true,
+      saveUninitialized: true,
+      cookie: { secure: false },
+    }
+  ));
   app.use(passport.session());
 };
