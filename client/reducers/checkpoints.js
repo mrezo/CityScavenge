aec22fd10c5537d021c46b3ed9c4b8a7743d71b1
Genevieve1722 <gsublette@gmail.com>
2016-05-16 11:59:33 -0700

    adds user actions to checkpoints reducer

--- client/reducers/checkpoints.js
+++ client/reducers/checkpoints.js
@@ -1,3 +1,18 @@
+const user = (state = {}, action) => {
+  switch (action.type) {
+    case 'FINISHPOINT_COLLISION':
+      if (state.title !== action.title) {
+        return state;
+      }
+      return Object.assign({}, state, {
+        collision: !state.collision,
+        timeIn: action.timeIn,
+      });
+    default:
+      return state;
+  }
+};
+
 const checkpoints = (state = [], action) => {
   switch (action.type) {
     case 'PLACE_CHECKPOINT':
