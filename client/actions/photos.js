ae95d3b103082a2eb28d90f1ed3fcda7023ab360
Genevieve1722 <gsublette@gmail.com>
2016-05-16 19:41:29 -0700

    adds action to check collision

--- client/actions/photos.js
+++ client/actions/photos.js
@@ -15,3 +15,27 @@ export const photoSubmit = (lat, lng, open) => {
     open,
   };
 };
+
+
+export const checkCollision = (dispatch) => {
+  fetch('api/v1/game/photosubmit', {
+    method: 'POST',
+    credentials: 'same-origin',
+  })
+  .then((response) => {
+    if (response.status >= 400) {
+      console.log('Server error', response);
+    }
+    return response;
+  })
+  .then((data) => {
+    // get user position
+    // check all checkpoints
+    // if collision, change state to true
+    // else, emit user error: cannot upload photo at this time
+  })
+  .catch((error) => {
+    console.log('Error', error);
+    return;
+  });
+};
