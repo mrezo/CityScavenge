9f3b47224c241fb7a96a06d331d2b11ce4b6352c
Genevieve1722 <gsublette@gmail.com>
2016-05-17 11:58:54 -0700

    passes dispatch to check collision

--- client/components/PhotoUpload.js
+++ client/components/PhotoUpload.js
@@ -5,7 +5,7 @@ import RaisedButton from 'material-ui/lib/raised-button';
 
 // TODO: add "action: " to form component with post request to DB
 
-const PhotoUpload = ({ checkCollision, checkpoints, currentUser, lat, lng, open, photoUploadStart, photoSubmit }) => (
+const PhotoUpload = ({ checkCollision, checkpoints, currentUser, dispatch, lat, lng, open, photoUploadStart, photoSubmit }) => (
   <div>
     <RaisedButton
       label="Check In"
@@ -26,7 +26,7 @@ const PhotoUpload = ({ checkCollision, checkpoints, currentUser, lat, lng, open,
           onClick={(e) => {
             e.preventDefault();
             photoSubmit(lat, lng, open);
-            checkCollision(checkpoints, currentUser);
+            checkCollision(dispatch, checkpoints, currentUser);
           }}
         />
       </form>
