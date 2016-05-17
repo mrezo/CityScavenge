5a4142936e6a9e416a62f3e5c8f74b1044cd4b7f
mrezo <balarezo.m@gmail.com>
2016-05-17 11:42:57 -0700

    Renames googleMap to map

--- client/reducers/mapReducer.js
+++ client/reducers/mapReducer.js
@@ -2,7 +2,7 @@ const mapReducer = (state = {}, action) => {
   switch (action.type) {
     case 'CREATE_MAP':
       return Object.assign({}, state, {
-        map: action.googleMap,
+        map: action.map,
         centerLat: action.lat,
         centerLng: action.lng,
       });
