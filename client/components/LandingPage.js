92615b1e52ae0575608d4af617bfcbfa18219af7
mrezo <balarezo.m@gmail.com>
2016-05-17 21:33:52 -0700

    Removes card element and adds container div

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -7,12 +7,12 @@ import CardTitle from 'material-ui/lib/card/card-title';
 
 const LandingPage = () => (
   <div className="landing-container">
-    <card backgroundColor={styles.Colors.cyan300}>
-      <CardMedia overlay={<CardTitle title="City Scavenge" subtitle="Explore your city" />}><img src={'https://cdn.getyourguide.com/niwziy2l9cvz/1XBkFZIKqYw0248uGCuaWG/cc3d1c8fec71b6706ac961b449d2d823/san-francisco-san-francisco-bay-1112x630.jpg'} /></CardMedia>
-      <div className="button-group">
+    <div className="transparent">
+      <span className="title">City Scavenge</span>
+      <div className="login-button">
         <RaisedButton label="Login with Google" backgroundColor={styles.Colors.red500} primary={true} linkButton={true} href="/api/v1/auth/google" />
       </div>
-    </card>
+    </div>
   </div>
 );
 
