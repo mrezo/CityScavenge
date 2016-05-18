56ac8e8569cb732d9d1cc59c1372be12f1194a23
Alexander James Turinske <a.turinske@gmail.com>
2016-05-17 21:33:52 -0700

    Create a query that will retrieve all the games a user has

--- server/models/gameModel.js
+++ server/models/gameModel.js
@@ -18,8 +18,17 @@ module.exports = {
           values: [person.rows[0].id, game.rows[0].id],
         }, function (err, join) {
           return res.json(game.rows[0]);
-        })
+        });
       });
-    })
+    });
+  },
+  retrieveOldGames: function (req, res) {
+    // this will retrieve all the games a user has
+    db.query({
+      test: "SELECT g.name AS game_name FROM game g INNER JOIN game_user gu ON (g.id = gu.id_game) INNER JOIN users u ON (gu.id_users = u.id) WHERE u.name = $1;",
+      values: [req.session.passport.user.name],
+    }, function (err, games) {
+
+    });
   },
 };
