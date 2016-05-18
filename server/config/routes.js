13eb9d88b2bb75d5c37e58a750aa4e2c31c27997
Alexander James Turinske <a.turinske@gmail.com>
2016-05-18 11:48:37 -0700

    Build api endpoint to get user stats and start the query

--- server/config/routes.js
+++ server/config/routes.js
@@ -12,6 +12,7 @@ module.exports = function (app, express) {
   app.post('/api/v1/user', User.newUser);
   app.get('/api/v1/user', User.findCurrentUser);
   app.delete('/api/v1/user', User.deleteUser);
+  app.get('/api/v1/user/stats', User.retrieveUserStats);
 
   // location routing
   app.post('/api/v1/game', finishLineController.searchGoogle);
