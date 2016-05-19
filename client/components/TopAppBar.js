2d1a755144b82829ef54d562961e0d5cf9586568
mrezo <balarezo.m@gmail.com>
2016-05-19 12:27:37 -0700

    Adds avatar to the top app bar next to user name

--- client/components/TopAppBar.js
+++ client/components/TopAppBar.js
@@ -1,20 +1,26 @@
 import React, { PropTypes } from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 import FlatButton from 'material-ui/lib/flat-button';
+import Avatar from 'material-ui/lib/avatar';
 
-const TopAppBar = ({ handleToggle, name }) => {
+const TopAppBar = ({ handleToggle, name, avatar }) => {
   return (
     <div>
-    <AppBar
-      title="City Scavenge"
-      onLeftIconButtonTouchTap={handleToggle}
-      iconElementRight={<FlatButton label={name} />}
-    />
-  </div>
+      <AppBar
+        title="City Scavenge"
+        onLeftIconButtonTouchTap={handleToggle}
+        iconElementRight={
+        <div>
+          <FlatButton label={name} />
+          <Avatar className="avatar" src={avatar} />
+        </div>}
+      />
+    </div>
   );
 };
 
 TopAppBar.propTypes = {
+  avatar: PropTypes.string,
   handleToggle: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
 };
