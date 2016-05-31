d68d4a0bd503da5c199c89c62a82f1eae41d65b6
mrezo <balarezo.m@gmail.com>
2016-05-31 08:53:54 -0700

    Modifies styles to have media query for mobile specific screen size

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -3,13 +3,18 @@ import RaisedButton from 'material-ui/lib/raised-button';
 import styles from 'material-ui/lib/styles';
 
 const LandingPage = () => (
-  <div className="landing-container">
-    <br></br>
-    <br></br>
-    <span className="title">City Scavenge</span>
-    <img className="icon" width="200px" height="200px" src="../assets/icon2.png"></img>
-    <div className="button-container">
-      <RaisedButton label="Login with Google" backgroundColor={styles.Colors.pink500} primary={true} linkButton={true} href="/api/v1/auth/google" />
+  <div className="main-container">
+    <div className="landing-container">
+      <span>City Scavenge</span>
+    </div>
+    <div className="mobile-landing-container">
+      <br></br>
+      <br></br>
+      <span className="mobile-title">City Scavenge</span>
+      <img className="mobile-icon" width="200px" height="200px" src="../assets/icon2.png"></img>
+      <div className="mobile-button-container">
+        <RaisedButton label="Login with Google" backgroundColor={styles.Colors.pink500} primary={true} linkButton={true} href="/api/v1/auth/google" />
+      </div>
     </div>
   </div>
 );
